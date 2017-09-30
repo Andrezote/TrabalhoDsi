234d3cd18fb5e0db49016c9df412865da87fd2af
Pedro Dorini <pedro.dorini@hotmail.com>
2017-09-30 16:50:24 -0300

    algumas correções

--- client/src/app/deletar-modal/deletar-modal.component.ts
+++ client/src/app/deletar-modal/deletar-modal.component.ts
@@ -4,42 +4,44 @@ import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 import {NgModule} from '@angular/core';
 import { Http } from '@angular//http';
 import {
-  FormGroup,
-  FormControl,
-  FormBuilder,
-  Validators
+    FormGroup,
+    FormControl,
+    FormBuilder,
+    Validators
 } from '@angular/forms';
 
 @Component({
-  selector: 'app-deletar-modal',
-  templateUrl: './deletar-modal.component.html',
-  styleUrls: ['./deletar-modal.component.css']
+    selector: 'app-deletar-modal',
+    templateUrl: './deletar-modal.component.html',
+    styleUrls: ['./deletar-modal.component.css']
 })
 export class DeletarModalComponent implements OnInit {
     public modalRef: BsModalRef;
     formulario : FormGroup;
-      constructor(
-          private modalService: BsModalService,
-          private formBuilder :FormBuilder,
-          private http: Http
-      ) {}
+    constructor(
+        private modalService: BsModalService,
+        private formBuilder :FormBuilder,
+        private http: Http
+    ) {}
 
-      public openModal(template: TemplateRef<any>) {
+    public openModal(template: TemplateRef<any>) {
         this.modalRef = this.modalService.show(template);
-      }
+    }
 
-      ngOnInit(){
-          this.formulario = this.formBuilder.group({
-          id_animal: [null]
-          })
-      }
+    ngOnInit(){
+        this.formulario = this.formBuilder.group({
+            id_animal: [null]
+        })
+    }
 
-      onSubmit(){
-          const body = this.formulario.value
+    onSubmit(){
+        const body = this.formulario.value
 
-          this.http
-            .post('http://localhost:3000/app/animalDel', body)
-            .subscribe()
-      }
+        this.http
+        .post('http://localhost:3000/app/animalDel', body)
+        .subscribe()
+
+        this.modalRef.hide();
+    }
 
 }
